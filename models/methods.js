d3b8aeda1df14d3e7c3be0e7f1bd9ae550c5adf3
Rodolfo Padilla <r.padilla.ruiz@gmail.com>
2013-11-25 10:35:41 -0500

    Update methods.js

--- models/methods.js
+++ models/methods.js
@@ -1,13 +1,21 @@
+//Crockfords "true" prototypal inheritance. 
+if (typeof Object.create !== 'function') {
+    Object.create = function (o) {
+        function F() {}
+        F.prototype = o;
+        return new F();
+    };
+} 
+
 var train = {
-"stops": {
+    "stops": {
         "101010": {
             "name": "San José Atlántico",
             "long_name": "San José Atlántico",
             "locality": "Central, San José",
             "search_words": "San José Atlántico",
             "location": [
-                9.93517,
-                -84.06859
+                    9.93517, -84.06859
             ]
         },
         "101030": {
@@ -16,8 +24,7 @@ var train = {
             "locality": "Central, San José",
             "search_words": "San José, Cementerio",
             "location": [
-                9.92702,
-                -84.09178
+                    9.92702, -84.09178
             ]
         },
         "101031": {
@@ -26,8 +33,7 @@ var train = {
             "locality": "Central, San José",
             "search_words": "San José Pacífico",
             "location": [
-                9.92559,
-                -84.08094
+                    9.92559, -84.08094
             ]
         },
         "101040": {
@@ -36,8 +42,7 @@ var train = {
             "locality": "Central, San José",
             "search_words": "San José, Plaza González Víquez",
             "location": [
-                9.92496,
-                -84.07436
+                    9.92496, -84.07436
             ]
         },
         "101041": {
@@ -46,8 +51,7 @@ var train = {
             "locality": "Central, San José",
             "search_words": "San José, Procuradoría",
             "location": [
-                9.9314,
-                -84.07217
+                    9.9314, -84.07217
             ]
         },
         "101080": {
@@ -56,8 +60,7 @@ var train = {
             "locality": "Central, San José",
             "search_words": "San José, La Sabana-Contraloría",
             "location": [
-                9.93265,
-                -84.10135
+                    9.93265, -84.10135
             ]
         },
         "101081": {
@@ -66,8 +69,7 @@ var train = {
             "locality": "Central, San José",
             "search_words": "San José, La Sabana-Estadio",
             "location": [
-                9.93352,
-                -84.11009
+                    9.93352, -84.11009
             ]
         },
         "101090": {
@@ -76,8 +78,7 @@ var train = {
             "locality": "Pavas, San José",
             "search_words": "Pavas, Metrópolis",
             "location": [
-                9.95885,
-                -84.153
+                    9.95885, -84.153
             ]
         },
         "101091": {
@@ -86,8 +87,7 @@ var train = {
             "locality": "Pavas, San José",
             "search_words": "Pavas, Demasa",
             "location": [
-                9.95012,
-                -84.14411
+                    9.95012, -84.14411
             ]
         },
         "101092": {
@@ -96,8 +96,7 @@ var train = {
             "locality": "Pavas, San José",
             "search_words": "Pavas Centro",
             "location": [
-                9.93997,
-                -84.1338
+                    9.93997, -84.1338
             ]
         },
         "101093": {
@@ -106,8 +105,7 @@ var train = {
             "locality": "Pavas, San José",
             "search_words": "Pavas, Jack's",
             "location": [
-                9.9402,
-                -84.12382
+                    9.9402, -84.12382
             ]
         },
         "101094": {
@@ -116,8 +114,7 @@ var train = {
             "locality": "Pavas, San José",
             "search_words": "Pavas, Rohrmoser",
             "location": [
-                9.93794,
-                -84.11953
+                    9.93794, -84.11953
             ]
         },
         "101095": {
@@ -126,8 +123,7 @@ var train = {
             "locality": "Pavas, San José",
             "search_words": "Pavas, Pecosa",
             "location": [
-                9.94614,
-                -84.13763
+                    9.94614, -84.13763
             ]
         },
         "113050": {
@@ -136,8 +132,7 @@ var train = {
             "locality": "Tibás, San José",
             "search_words": "Tibás, Cuatro Reinas",
             "location": [
-                9.95557,
-                -84.08669
+                    9.95557, -84.08669
             ]
         },
         "115050": {
@@ -146,8 +141,7 @@ var train = {
             "locality": "Montes de Oca, San José",
             "search_words": "Montes de Oca, San Pedro-UCR",
             "location": [
-                9.93469,
-                -84.05436
+                    9.93469, -84.05436
             ]
         },
         "115051": {
@@ -156,8 +150,7 @@ var train = {
             "locality": "Montes de Oca, San José",
             "search_words": "Montes de Oca, Universidad Latina",
             "location": [
-                9.9326,
-                -84.04386
+                    9.9326, -84.04386
             ]
         },
         "118010": {
@@ -166,8 +159,7 @@ var train = {
             "locality": "Curridabat, San José",
             "search_words": "Curridabat, Freses-CFIA",
             "location": [
-                9.92707,
-                -84.03885
+                    9.92707, -84.03885
             ]
         },
         "118011": {
@@ -176,8 +168,7 @@ var train = {
             "locality": "Curridabat, San José",
             "search_words": "Curridabat, Cipreses-UACA",
             "location": [
-                9.91955,
-                -84.02433
+                    9.91955, -84.02433
             ]
         },
         "301020": {
@@ -186,8 +177,7 @@ var train = {
             "locality": "Cartago, Cartago",
             "search_words": "Cartago",
             "location": [
-                9.86667,
-                -83.92216
+                    9.86667, -83.92216
             ]
         },
         "303060": {
@@ -196,8 +186,7 @@ var train = {
             "locality": "La Unión, Cartago",
             "search_words": "La Unión, Tres Ríos",
             "location": [
-                9.91054,
-                -83.98657
+                    9.91054, -83.98657
             ]
         },
         "401010": {
@@ -206,8 +195,7 @@ var train = {
             "locality": "Heredia, Heredia",
             "search_words": "Heredia",
             "location": [
-                9.99467,
-                -84.11713
+                    9.99467, -84.11713
             ]
         },
         "403060": {
@@ -216,8 +204,7 @@ var train = {
             "locality": "Santo Domingo, Heredia",
             "search_words": "Santo Domingo, Santa Rosa",
             "location": [
-                9.97213,
-                -84.09828
+                    9.97213, -84.09828
             ]
         },
         "407010": {
@@ -226,8 +213,7 @@ var train = {
             "locality": "Belén, Heredia",
             "search_words": "Belén, San Antonio",
             "location": [
-                9.9785,
-                -84.18496
+                    9.9785, -84.18496
             ]
         },
         "409020": {
@@ -236,30 +222,30 @@ var train = {
             "locality": "San Pablo, Heredia",
             "search_words": "San Pablo, Miraflores",
             "location": [
-                9.98734,
-                -84.10892
+                    9.98734, -84.10892
             ]
         }
-    }};
+    }
+};
 //gets an array of stoptimes objects
 /*
-var getStopTimes  = function(trip) {
-        var tripStops = [],
-            len = train.stoptimes.length;
-        for (var i = 0, l = len, found = false; i < l; i++) {
-            if (train.stoptime[i].trip == trip) {
-                found = true;
-                tripStops.push(train.stoptime[i]);
-            }
-        }
-        if (found) {
-            //console.log(" this Trip is: " + stoptimes);
-            console.log(" Este viaje en tren tiene " + tripStops.length + " paradas");
-            console.log(tripStops);
-        } else {
-            console.log("no stops.");
-        };
-    }
+var getStopTimes = function(trip) {
+var tripStops = [],
+len = train.stoptimes.length;
+for (var i = 0, l = len, found = false; i < l; i++) {
+if (train.stoptime[i].trip == trip) {
+found = true;
+tripStops.push(train.stoptime[i]);
+}
+}
+if (found) {
+//console.log(" this Trip is: " + stoptimes);
+console.log(" Este viaje en tren tiene " + tripStops.length + " paradas");
+console.log(tripStops);
+} else {
+console.log("no stops.");
+};
+}
 getStopTimes(90);*/
 
 //Transforming stops into geojson objects
@@ -275,45 +261,43 @@ var stopMarker = {
     },
     "geometry": {
         "type": "Point",
-        "coordinates": [
-        ]
+        "coordinates": []
     }
 };
 
-console.log(stopMarker);
 
-var moveData = function(){
-    var newStops =[];
+var moveData = function () {
+    var newStops = [];
     var stops = train.stops;
     var Marker = Object.create(stopMarker);
-    
+
     for (var id in stops) {
-       var obj = stops[id];
-       console.log("Handling propertie of stop: "+ id);
-       Marker.properties.id = stops[id]
-       for (var prop in obj) {
-    
-      if(obj.hasOwnProperty(prop)){
-       console.log(">>>>  "+ prop + " = " + obj[prop]);
-       
-        Marker.properties.name = obj.name;
-        Marker.properties.long_name = obj.long_name;
-        Marker.properties.id = obj.locality;
-        Marker.properties.id = obj.search_words;
-        Marker.geometry.coordinates = obj.location;
+        var obj = stops[id];
+        console.log("Handling propertie of stop: " + id);
+        Marker.properties.id = stops[id];
+        for (var prop in obj) {
 
+            if (obj.hasOwnProperty(prop)) {
+                console.log(">>>> " + prop + " = " + obj[prop]);
 
-      }
-      newStops.push(Marker);
-   }
-   console.log(Marker)
-   return newStops;
-}
-}
+                Marker.properties.name = obj.name;
+                Marker.properties.long_name = obj.long_name;
+                Marker.properties.locality = obj.locality;
+                Marker.properties.search_words = obj.search_words;
+                Marker.geometry.coordinates = obj.location;
+				console.log(Marker);
+
+            }
+            newStops.push(Marker);
+        }
+        
+        return newStops;
+    }
+};
 
 
 //Current stop object
-"stops" :{
+/*"stops": {
     "101010": {
         "name": "San José Atlántico",
         "long_name": "San José Atlántico",
@@ -323,23 +307,22 @@ var moveData = function(){
             "lat": "9.93517",
             "lon": "-84.06859"
         }
-        
-        
-//Geojson format 
-var geojsonSample= {
-    "type": "Feature",
-    "properties": {
-        "Hello": "World"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [
-            -84.16656017303465,
-            9.969104212901023
-        ]
-    }
-};
 
 
+        //Geojson format
+        var geojsonSample = {
+            "type": "Feature",
+            "properties": {
+                "Hello": "World"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-84.16656017303465,
+                        9.969104212901023
+                ]
+            }
+        };
 
-moveData();
+*/
+
+        moveData();
