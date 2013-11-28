0a95707273de3434f8b1b5906907d7a892bd77f6
Rodolfo Padilla <r.padilla.ruiz@gmail.com>
2013-11-28 08:00:15 -0600

    added filter for stoptimes

--- models/methods.js
+++ models/methods.js
@@ -1,7 +1,18 @@
 
 //gets an array of stoptimes objects
 //this is broke!!!!
-
+var stoptimes ={
+        
+        "id    ": 50641112,
+        "train_type": "Apolo",
+        "trip": 1,
+        "stop_sequence": 2,
+        "headsign": "San José Atlántico",
+        "arrival_time": "06:05",
+        "departure_time": "06:05",
+        "route_id": 506411,
+        "stop_id": 409020
+    };
 
 var trips = require('../public/models/trips'),
 	routes = require('../public/models/routes'),
@@ -12,25 +23,31 @@ var trips = require('../public/models/trips'),
 var getStopTimes = function(trip) {
 var tripStops = [],
 len = stoptimes.length,
-t=trips.length;
+
 console.log(trips.length);
+found = false;
 
-for (var i = 0, x = tripStops.length,  l = len, found = false; i < l, x<t ; i++) {
-	console.log(stoptimes[i].trip + "  " +trips[x].number)
-if (stoptimes[i].trip == trips[x].number) {
-found = true;
-tripStops.push(stoptimes[i]);
-}
-}
-if (found) {
-//console.log(" this Trip is: " + stoptimes);
-console.log(" Este viaje en tren tiene " + tripStops.length + " paradas");
-//console.log(tripStops);
-} else {
-	console.log(tripStops.length);
-console.log("no stops. trip # is: "+ trips[trip] );
-}
-};
+var  filterStoptimes  =   function(stoptime, trip){
+    var  found = false;
+    if (stoptime.trip===trip.number){
+    found=true;
+    }
+    else {
+    console.log("no stops. trip # is: "+ trips[trip] );
+    }
 
+ };  
+ 
+var getStopTimes  = function( trip ) {
+    var  l =  trip.length;
+    for (var i = 0; i < len; i++) {
+    for( var j=0; j<l; j++ ) {
+        filterStoptimes(stoptimes[i], trip[j]);
+        if (found){
+            tripStops.push(stoptimes[i]};
+        }
+    }
+    return tripStops;
+}
 
-getStopTimes(trips[1]);
+console.log(getStopTimes(trips[1]));
