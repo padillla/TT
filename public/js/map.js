b4cb1974d8c33114d8fa6e200ad98542f8be3186
Rodolfo Padilla Ruiz <r.padilla.ruiz@gmail.com>
2014-02-08 01:20:56 -0600

    Moving markers for train done, TODO: make it nice looking

--- public/js/map.js
+++ public/js/map.js
@@ -14,15 +14,13 @@
        layers: [cloudmade],
        zoomControl: false
      });
+     var routes = [];
 
-     var trips = {};
-     var stops = [];
-     var routes = {};
 
      var tinyIcon = L.Icon.extend({
        options: {
          shadowUrl: "../img/marker-shadow.png",
-         iconSize: [30, 35],
+         iconSize: [25, 35],
          iconAnchor: [12, 36],
          shadowSize: [41, 41],
          shadowAnchor: [12, 38],
@@ -33,15 +31,15 @@
      var trainIcon = new tinyIcon({
        iconUrl: "../img/train.png"
      });
-     
+
      var railIcon = new tinyIcon({
        iconUrl: "../img/levelcrossing.png"
      });
-     
+
      var movingTrainIcon = new tinyIcon({
        iconUrl: "../img/steamtrain.png"
      });
-    
+
      var redIcon = new tinyIcon({
        iconUrl: "../img/marker-red.png"
      });
@@ -49,7 +47,7 @@
        iconUrl: "../img/marker-yellow.png"
      });
 
-  
+
 
      //push data to map
 
@@ -64,13 +62,13 @@
      $.ajax({
        url: '/stops',
        success: function(data) {
-        
+
          L.geoJson(data, {
            pointToLayer: function(f, latlng) {
-            
+
              return new L.Marker(latlng, {
 
-               icon: railIcon
+               icon: yellowIcon
              }).bindPopup('Nombre: ' + f.properties.long_name + '<br>Localidad: ' + f.properties.locality + '<br>Localizacion: ' + f.geometry.coordinates);
            }
          }).addTo(map);
@@ -83,15 +81,70 @@
 
          for (var i in data) {
            if (data.hasOwnProperty(i)) {
+             L.geoJson(data[i]).addTo(map);
+             routes.push(data[i].geometry.coordinates);
 
-             L.geoJson(data[i], {
-
-             }).addTo(map);
            }
          }
+         animateTrains(routes)
        }
-
      });
 
-// TODO:  Add animated marker for moving train
+     var animateTrains = function(latLngs) {
+
+
+       var routeLines = [],
+         markers = [];
+
+       $.each(latLngs, function(i, line) {
+
+         line = invertLatLngs(line);
+         var polyline = L.polyline(line);
+         routeLines.push(polyline);
+       });
+
+
+
+       $.each(routeLines, function(i, routeLine) {
+         var marker = L.animatedMarker(routeLine.getLatLngs(), {
+           icon: movingTrainIcon,
+           autoStart: false,
+           /*onEnd: function() {
+             $(this._shadow).fadeOut();
+             $(this._icon).fadeOut(3000, function() {
+               map.removeLayer(this);
+             });
+           }*/
+         });
+
+         map.addLayer(marker);
+         markers.push(marker);
+       });
+
+       $(function() {
+         $('#start').click(function() {
+           console.log('start');
+           $.each(markers, function(i, marker) {
+             marker.start();
+           });
+
+         });
+       });
+     };
+
+     ///Ugly function to invert lat lng on routes, 
+     //when these are inverted and leaflet position the markers 
+     //in the south pole, I dont know why the hell it does that.
+     var invertLatLngs = function(latLngs) {
+       var correctedArray = [];
+       $.each(latLngs, function(i, innerlatlng) {
+         var reversedLatlng = innerlatlng.reverse();
+         correctedArray.push(reversedLatlng);
+
+       });
+       return correctedArray;
+     }
+
+
    });
+    // TODO:  Add animated marker for moving train
